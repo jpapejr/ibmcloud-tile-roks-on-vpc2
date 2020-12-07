8934f1a381dd9756e7714c67ded8c5e34de00763
jpapejr <jpapejr@icloud.com>
2020-12-07 17:36:40 -0500

    Remove worker_pool_name from module

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ module "cluster_and_workerpool" {
   region = var.region
   resource_group = var.resource_group
   cluster_name = var.cluster_name
-  worker_pool_name = var.worker_pool_name
   cos_instance_crn = module.cos.cos_instance_crn
   entitlement = var.entitlement
 }
