8989ea96017f53c5419fe6791a03878283af0eba
jpapejr <jpapejr@icloud.com>
2020-12-08 10:24:07 -0500

    Refactor to remove modules

--- main.tf
+++ main.tf
@@ -1,18 +1,130 @@
-module "cos" {
-  source =  "./cloud_object_store"
-  service_instance_name = var.cos_service_name
-  plan = var.cos_service_plan
+resource "ibm_resource_instance" "cos_instance" {
+  name     = var.cos_service_name
+  service  = "cloud-object-storage"
+  plan     = var.cos_service_plan
+  location = "global"
 }
 
-module "cluster_and_workerpool" {
-  source = "./roks_on_vpc"
-  flavor = var.cluster_node_flavor
-  kube_version = var.cluster_kube_version
-  worker_count = var.default_worker_pool_count
-  region = var.region
-  resource_group = var.resource_group
-  cluster_name = var.cluster_name
-  cos_instance_crn = module.cos.cos_instance_crn
-  entitlement = var.entitlement
+resource "random_id" "name1" {
+  byte_length = 2
 }
 
+resource "random_id" "name2" {
+  byte_length = 2
+}
+
+resource "random_id" "name3" {
+  byte_length = 2
+}
+
+locals {
+  ZONE1 = "${var.region}-1"
+  ZONE2 = "${var.region}-2"
+  ZONE3 = "${var.region}-3"
+}
+
+resource "ibm_is_vpc" "vpc1" {
+  name = "vpc-${random_id.name1.hex}"
+}
+
+resource "ibm_is_public_gateway" "testacc_gateway1" {
+    name = "public-gateway1"
+    vpc = ibm_is_vpc.vpc1.id
+    zone = local.ZONE1
+}
+
+resource "ibm_is_public_gateway" "testacc_gateway2" {
+    name = "public-gateway2"
+    vpc = ibm_is_vpc.vpc1.id
+    zone = local.ZONE2
+}
+
+resource "ibm_is_public_gateway" "testacc_gateway3" {
+    name = "public-gateway3"
+    vpc = ibm_is_vpc.vpc1.id
+    zone = local.ZONE3
+}
+
+resource "ibm_is_security_group_rule" "testacc_security_group_rule_tcp" {
+    group = ibm_is_vpc.vpc1.default_security_group
+    direction = "inbound"
+    tcp {
+        port_min = 30000
+        port_max = 32767
+    }
+ }
+
+resource "ibm_is_subnet" "subnet1" {
+  name                     = "subnet-${random_id.name1.hex}"
+  vpc                      = ibm_is_vpc.vpc1.id
+  zone                     = local.ZONE1
+  total_ipv4_address_count = 256
+  public_gateway = ibm_is_public_gateway.testacc_gateway1.id
+}
+
+resource "ibm_is_subnet" "subnet2" {
+  name                     = "subnet-${random_id.name2.hex}"
+  vpc                      = ibm_is_vpc.vpc1.id
+  zone                     = local.ZONE2
+  total_ipv4_address_count = 256
+  public_gateway = ibm_is_public_gateway.testacc_gateway2.id
+}
+
+resource "ibm_is_subnet" "subnet3" {
+  name                     = "subnet-${random_id.name3.hex}"
+  vpc                      = ibm_is_vpc.vpc1.id
+  zone                     = local.ZONE3
+  total_ipv4_address_count = 256
+  public_gateway = ibm_is_public_gateway.testacc_gateway3.id
+}
+
+data "ibm_resource_group" "resource_group" {
+  name = var.resource_group
+}
+
+resource "ibm_container_vpc_cluster" "cluster" {
+  name              = "${var.cluster_name}-${random_id.name1.hex}"
+  vpc_id            = ibm_is_vpc.vpc1.id
+  kube_version      = var.cluster_kube_version
+  flavor            = var.cluster_node_flavor
+  worker_count      = var.worker_count
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  entitlement       = var.entitlement
+  cos_instance_crn  = ibm_resource_instance.cos_instance.id
+  wait_till         = "OneWorkerNodeReady"
+
+  zones {
+    subnet_id = ibm_is_subnet.subnet1.id
+    name      = local.ZONE1
+  }
+
+
+}
+
+resource "ibm_container_vpc_worker_pool" "cluster_pool2" {
+  cluster           = ibm_container_vpc_cluster.cluster.id
+  worker_pool_name  = "default2"
+  flavor            = var.flavor
+  vpc_id            = ibm_is_vpc.vpc1.id
+  worker_count      = var.default_worker_pool_count
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  entitlement       = var.entitlement
+  zones {
+    name      = local.ZONE2
+    subnet_id = ibm_is_subnet.subnet2.id
+  }
+}
+
+resource "ibm_container_vpc_worker_pool" "cluster_pool3" {
+  cluster           = ibm_container_vpc_cluster.cluster.id
+  worker_pool_name  = "default3"
+  flavor            = var.flavor
+  vpc_id            = ibm_is_vpc.vpc1.id
+  worker_count      = var.worker_count
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  entitlement       = var.entitlement
+  zones {
+    name      = local.ZONE3
+    subnet_id = ibm_is_subnet.subnet3.id
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@
 output "cos_instance_crn" {
   value = ibm_resource_instance.cos_instance.id
 }
+
+output "cluster_id" {
+  value = ibm_container_vpc_cluster.cluster.id
+}
